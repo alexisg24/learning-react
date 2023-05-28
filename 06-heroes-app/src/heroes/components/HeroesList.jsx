1f181d73127d87a33a59cc80f6bfa52c30c824ec
Alexis Garcia <azoreno2@gmail.com>
2023-05-28 11:34:11 -0400

    :sparkles: Hero info and useMemo
    Hero info and useMemo added in static info

--- 06-heroes-app/src/heroes/components/HeroesList.jsx
+++ 06-heroes-app/src/heroes/components/HeroesList.jsx
@@ -1,8 +1,9 @@
 import { HeroCard } from './HeroCard'
 import { getHeroesByPublisher } from '../helpers'
+import { useMemo } from 'react'
 
 const HeroesList = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher)
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher])
   return (
     <div className='row row-cols-q row-cols-md-3 g-3'>
       {
--- 06-heroes-app/src/heroes/pages/Hero.jsx
+++ 06-heroes-app/src/heroes/pages/Hero.jsx
@@ -1,8 +1,52 @@
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
+import { getHeroByID } from '../helpers'
+import { useMemo } from 'react'
+
 const HeroPage = () => {
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const hero = useMemo(() => getHeroByID(id), [id])
+  const url = useMemo(() => `/assets/heroes/${id}.jpg`, [id])
+  const handleNavitageBack = () => {
+    return navigate(-1)
+  }
+
+  if (!hero) {
+    return (<Navigate to='/marvel' />)
+  }
   return (
-    <>
-      <h1>HeroPage</h1>
-    </>
+    <section className='row mt-5'>
+      <div className='col-4'>
+        <img
+          src={url}
+          alt={hero.superhero}
+          className='img-thumbnail'
+        />
+      </div>
+      <div className='col-8'>
+        <h3>{hero.superhero}</h3>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'><b>Alter ego:</b> {hero.alter_ego}</li>
+          <li className='list-group-item'><b>Publisher:</b> {hero.publisher}</li>
+          <li className='list-group-item'><b>First Appearance:</b> {hero.first_appearance}</li>
+        </ul>
+
+        {
+        (hero.characters !== hero.alter_ego) &&
+        (
+          <>
+            <h5 className='mt-3'>Characters</h5>
+            <p>{hero.characters}</p>
+          </>
+        )
+        }
+        <button
+          className='btn btn-outline-primary'
+          onClick={handleNavitageBack}
+        >Back
+        </button>
+      </div>
+    </section>
   )
 }
 
