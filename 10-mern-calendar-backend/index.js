8650b0bb8ce274621d8a65448f64ccae952f3f4a
Alexis Garcia <azoreno2@gmail.com>
2023-07-25 19:13:53 -0400

    :rocket: Fix Static Routes Errors

--- 10-mern-calendar-backend/index.js
+++ 10-mern-calendar-backend/index.js
@@ -11,6 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/events', require('./routes/events'))
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en el puerto ${process.env.PORT}`)
